d817bd0d95f4529b22691415417fd11635c161f3
Carlos Valverde <cevc1184@gmail.com>
2022-08-06 11:49:44 -0600

    First Component

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+})
+export class CounterComponent {
+  public title: string = 'Componente contador';
+  public contador: number = 0;
+  public numeros: number[] = [];
+
+  modificar = (cantidad: number) => {
+    this.contador += cantidad;
+    this.numeros.push(this.contador);
+    //console.log(this.numeros);
+  };
+
+  // restar() {
+  //   this.contador--;
+  // }
+
+  // sumar = () => this.contador++;
+}
